ec50766e001f6f63891104bf5e08219b17a99288
hasan421 <hhhsonmez68@gmail.com>
2021-05-23 15:35:36 +0300

    Circle Calculate

---
+++ Circle/circle.js
@@ -0,0 +1,9 @@
+pi=3.14
+exports.circleArea=(r)=>{
+
+  return  circleArea=pi*r*r
+
+}
+exports.circleCircumference=(r)=>{
+    return circleCircumference=2*pi*r
+}
---
+++ Circle/index.js
@@ -0,0 +1,7 @@
+const circle=require('../Circle/circle')
+
+let dairenin_alani=circle.circleArea(5)
+let dairenin_cevresi=circle.circleCircumference(5)
+
+console.log(dairenin_alani);
+console.log(dairenin_cevresi);
